454a346009ea23f3e5d6c197a25a18e5059e3e06
muhameti1 <67862236+muhameti1@users.noreply.github.com>
2023-01-08 23:39:10 +0100

    new

--- src/layouts/Home/Profile.js
+++ src/layouts/Home/Profile.js
@@ -21,16 +21,13 @@ const ProfileText = ({ visible, titleId }) => (
       <DecoderText text="Hi there" start={visible} delay={500} />
     </Heading>
     <Text className={styles.description} data-visible={visible} size="l" as="p">
-      I’m Hamish, currently I live in Sydney working as a senior product designer at{' '}
-      <Link href="https://www.qwilr.com">Qwilr</Link>. My projects include UX design, UI
-      animations, and icon illustration. Being comfortable with code allows me to rapidly
-      prototype and validate experiences. If you’re interested in the tools and software I
-      use check out my <Link href="/uses">uses page</Link>.
+      We are Gates-IT, Digital agency, specialized in web design & development and social
+      media marketing. We help small businesses get started with their first website,
+      optimize and grow their existing website, and build a social media presence.
     </Text>
     <Text className={styles.description} data-visible={visible} size="l" as="p">
-      In my spare time I like to practice Brazilian Jiu Jitsu, play video games, and{' '}
-      <Link href="/projects/volkihar-knight">make mods</Link>. I’m always down for hearing
-      about new projects, so feel free to drop me a line.
+      We are your one-stop digital shop. We have digital marketing specialists with a
+      proven track record in delivering results and increasing ROI.
     </Text>
   </Fragment>
 );
